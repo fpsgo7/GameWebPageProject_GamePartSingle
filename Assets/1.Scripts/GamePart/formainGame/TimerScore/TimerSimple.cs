3ca6b09b678bdf05e51c55bae491dd97008c88c6
fpsgo7 <101778043+fpsgo7@users.noreply.github.com>
2024-01-18 16:05:40 +0900

    게임 점수를 위한 타이머 가지고오기
    게임 파트에서 만들어둔 타이머를 가져와 적용시킬것이다.
    
    TimerSimple : 활성화 되면 시간이 흐르며 남은시간 정보를 다른 클래스에서 가져와 점수로 활용한다.
    
    이후 코드를 수정하여 활용할 것이다.

---
+++ Assets/1.Scripts/GamePart/formainGame/TimerScore/TimerSimple.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class TimerSimple : MonoBehaviour
+{
+    private float time = 3600;
+    [SerializeField]
+    private TextMeshProUGUI timeWatchTMP;
+    char[] charArray = new char[7];
+
+    void Update()
+    {
+        if (time > 0.1)
+        {
+            time -= Time.deltaTime;
+            int userTime = (int)(time * 100);
+            charArray[4] = '.';
+            for (int i = charArray.Length - 1; i >= 5; i--)
+            {
+                // 배열의 마지막 부터 값이 들어간다.
+                charArray[i] = (char)((userTime % 10) + 48);// 아스키 코드로 숫자를 찾기위해 + 48
+                userTime /= 10;//10을 나눔으로써 가장 낮은 자리수 가 사라진다.
+            }
+            for (int i = charArray.Length-4 ; i >= 0; i--)
+            {
+                // 배열의 마지막 부터 값이 들어간다.
+                charArray[i] = (char)((userTime % 10) + 48);// 아스키 코드로 숫자를 찾기위해 + 48
+                userTime /= 10;//10을 나눔으로써 가장 낮은 자리수 가 사라진다.
+            }
+            timeWatchTMP.SetCharArray(charArray, 0, charArray.Length);
+        }
+    }
+
+    public int GetTime()
+    {
+        float answerTime = time * 100;
+        return (int)answerTime;
+    }
+}
+
